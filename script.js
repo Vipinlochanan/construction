5b147b449ad62caa06bee53686d9854e5b387013
vipin lochanan <vipinvl48@gmail.com>
2024-10-18 10:37:12 +0530

    Second commit coustruction

---
+++ script.js
@@ -0,0 +1,64 @@
+
+		$(document).ready(function()
+		{
+			$(".bg1").mouseover(function()
+			{
+				$(this).css("background-image", "url(./image/bg.jpg)")
+			});
+			$(".bg1").mouseout(function()
+			{
+				$(this).css("background-image", "")
+			});
+			$(".bg2").mouseover(function()
+			{
+				$(this).css("background-image", "url(./image/bg2.jpg)")
+			});
+			$(".bg2").mouseout(function()
+			{
+				$(this).css("background-image", "")
+			});
+			$(".bg3").mouseover(function()
+			{
+				$(this).css("background-image", "url(./image/bg3.jpg)")
+			});
+			$(".bg3").mouseout(function()
+			{
+				$(this).css("background-image", "")
+			});
+			$(".bg4").mouseover(function()
+			{
+				$(this).css("background-image", "url(./image/bg4.jpg)")
+			});
+			$(".bg4").mouseout(function()
+			{
+				$(this).css("background-image", "")
+			});
+			$(".bg5").mouseover(function()
+			{
+				$(this).css("background-image", "url(./image/bg5.jpg)")
+			});
+			$(".bg5").mouseout(function()
+			{
+				$(this).css("background-image", "")
+			});
+			$(".bg6").mouseover(function()
+			{
+				$(this).css("background-image", "url(./image/bg6.jpg)");
+			});
+			$(".bg6").mouseout(function()
+			{
+				$(this).css("background-image", "")
+			});
+			$(".bg7").mouseover(function()
+			{
+				$(this).css("background-image", "url(./image/bg7.jpg)");
+			});
+			$(".bg7").mouseout(function()
+			{
+				$(this).css("background-image", "");
+			});
+
+
+		});
+
+	
